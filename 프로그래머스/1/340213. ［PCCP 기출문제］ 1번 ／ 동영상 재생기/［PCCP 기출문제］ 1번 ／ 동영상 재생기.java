4a1dd4e9532a1c2d1157b2d26eb25ec4761cc477
DajeongLee <97433636+Dazunx@users.noreply.github.com>
2026-05-22 21:56:49 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 5.65 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -19,7 +19,6 @@ class Solution {
         opEnd[0] = Integer.parseInt(op_end.substring(0,2));
         opEnd[1] = Integer.parseInt(op_end.substring(3,5));
         
-        // [수정] 첫 오프닝 체크: 분/초를 올바르게 비교하도록 수정
         if (isOpening(curTime, opStart, opEnd)) {
             curTime[0] = opEnd[0];
             curTime[1] = opEnd[1];
@@ -34,7 +33,6 @@ class Solution {
                         curTime[1] += 60;
                         curTime[0]--;
                     }
-                    // [수정] 분이 내려간 후 최종 시간이 00:00 미만이 되면 완전히 0으로 고정
                     if(curTime[0] < 0) {
                         curTime[0] = 0;
                         curTime[1] = 0;
@@ -57,7 +55,6 @@ class Solution {
                     break;
             }
             
-            // [수정] 명령어 이후 오프닝 체크
             if (isOpening(curTime, opStart, opEnd)) {
                 curTime[0] = opEnd[0];
                 curTime[1] = opEnd[1];
@@ -67,9 +64,7 @@ class Solution {
         return String.format("%2s:%2s", curTime[0]+"", curTime[1]+"").replace(" ", "0");
     }
     
-    // [추가] 복잡했던 오프닝 조건문을 직관적이고 정확하게 분리한 헬퍼 함수
     private boolean isOpening(int[] cur, int[] start, int[] end) {
-        // 총 초 단위로 환산해서 비교하면 기존 배열 틀을 유지하면서도 완벽하게 정확해집니다.
         int curSec = cur[0] * 60 + cur[1];
         int startSec = start[0] * 60 + start[1];
         int endSec = end[0] * 60 + end[1];
